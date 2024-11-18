9e8c27820b04f0c1b4476c87bf21356e8653f049
placidenduwayo <placide.nd@gmail.com>
2024-11-18 13:48:15 +0100

    refac(): remove eureka as deploying in k8s cluster

--- exalt-backend/business-microservices/exalt-business-microservice-bankaccount/src/main/java/fr/exalt/businessmicroserviceaccount/infrastructure/adapters/input/web/BankAccountController.java
+++ exalt-backend/business-microservices/exalt-business-microservice-bankaccount/src/main/java/fr/exalt/businessmicroserviceaccount/infrastructure/adapters/input/web/BankAccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping(value = "/api-bank-account")
+@RequestMapping(value = "/api/bank-account")
 @RequiredArgsConstructor
 public class BankAccountController {
     //input adapter
